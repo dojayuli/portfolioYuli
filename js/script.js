d73bd425bdd265776310108b4d5dbdef79cfde49
dojayuli <liliraspitu@gmail.com>
2022-07-13 00:42:06 -0300

    finalizacion de proyecto

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ============================== typing animation ============================== */
+var typed = new Typed(".typing",{
+    strings:["","Web Designer", "web Developer", "Graphic Designer", "YouTuber"],
+    typeSpeed: 100,
+    BackSpeed:60,
+    loop:true
+})
